d44f59a37fdc2c8046bfadd72bf5eec6114dc33f
rishuxx <ripxjaws09@gmail.com>
2025-08-30 18:25:20 +0530

    Factorail_with_dowhileloop

--- fourth.cpp
+++ fourth.cpp
@@ -1,17 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main (){
+int main(){
 
     int a;
-    int fact;
+    char ch;
+    int fact = 1;
 
-    for (int i = 1; i <= a; i++){
+    do
+    {
+        cout << "Enter the number" << endl;
+        cin >> a;
+          
+        for (int i = 1; i <= a; i++)
+        {
+            fact *= i;
+        }
 
-        fact = fact * 1;
+      cout << "Factorial of number " << a << " is: " << fact << endl;
 
-    }
 
-    cout << "factorial = " << fact << endl;
-    
+
+        cout << "want to find for another number (y/n)" << endl;
+        cin >> ch;
+
+    } while (ch == 'y' || ch == 'Y');
+
+    cout << "you choosed wrong option Exiting ";
+
+    return 0;
 }
+
+
+
